3a3128f46d6b4278f070b646a97be805a3e45ba6
Guiteau <diegodam999@gmail.com>
2025-08-23 23:31:42 +0100

    feat(lib): yargs lib samples

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
-const getNotes = require('./notes.js');
-const chalk = require('chalk');
+const getNotes = require("./notes.js")
+const chalk = require("chalk")
+const yargs = require("yargs")
 
-const msg = getNotes();
-console.log(msg);
+// Customize yargs version
+yargs.version("1.1.0")
 
-console.log(chalk.green('Success!'));
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    console.log("Title: " + argv.title)
+    console.log("Body: " + argv.body)
+  },
+})
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: () => {
+    console.log("Removing the note!")
+  },
+})
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler: () => {
+    console.log("Listing out all notes!")
+  },
+})
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: () => {
+    console.log("Reading a note!")
+  },
+})
+
+// add, remove, read, list
+yargs.parse()
