be7747ce932e280b573edb6cfbf60207e5963749
Guiteau <diegodam999@gmail.com>
2025-10-15 23:52:59 +0200

    feat: 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
 
 app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
 
+app.get('/help', (req, res) => {
+  res.send('Help page');
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
+});
+
+app.get('*', (req, res) => {
+  res.send('404 Page Not Found');
 });
 
 module.exports = app;
